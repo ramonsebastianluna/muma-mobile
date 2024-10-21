c1d1dd74ce81a92426f8a575bb212d32f899d02f
Seba Luna <ramon.sebastian.luna@gmail.com>
2024-10-21 18:04:40 -0300

    feat(F01): initial mockup added

--- pages/StartedScreen/index.jsx
+++ pages/StartedScreen/index.jsx
@@ -1,13 +1,28 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 import Constants from 'expo-constants';
 
 
-const StartedScreen = () => {
+const StartedScreen = ({ navigation }) => {
   return (
     <View style={{marginTop: Constants.statusBarHeight}}>
-        <Text>Pantalla inicial</Text>
+      <TouchableOpacity
+        style={styles.createButtonText}
+        onPress={() => navigation.navigate("Login")}
+      >
+        <Text>Crear Cuenta</Text>
+      </TouchableOpacity>
     </View>
   )
 }
 
 export default StartedScreen
+
+const styles = StyleSheet.create({
+  createButtonText: {
+    backgroundColor: "#F6F6F6",
+    padding: 15,
+    borderColor: "#F08318",
+    borderRadius: 5,
+    alignItems: "center", 
+  },
+})
