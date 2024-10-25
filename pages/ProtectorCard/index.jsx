8ef8e76d96d93c568d68d533674b1b5b337640d9
HildaEncina <encina.hilda@gmail.com>
2024-10-25 14:18:00 -0300

    fix(F32) add styles

--- pages/ProtectorCard/index.jsx
+++ pages/ProtectorCard/index.jsx
@@ -12,27 +12,29 @@ const ProtectorCard = ({
   protectorInstagram = '@animalistasderosario',
 }) => {
   return (
-    <View style={styles.card}>
-      <TouchableOpacity onPress={() => { /* Aquí puedes manejar la navegación o acción */ }}>
-        <Image source={IconClose} style={styles.closeButton} />
-      </TouchableOpacity>
+    <View style={styles.container}>  
+          <View style={styles.card}>
+            <TouchableOpacity onPress={() => { /* Aquí puedes manejar la navegación o acción */ }}>
+              <Image source={IconClose} style={styles.closeButton} />
+            </TouchableOpacity>
 
-      <View style={styles.header}>
-        <Image source={ProtectorImg} style={styles.profileImage} />
-      </View>
+            <View style={styles.header}>
+              <Image source={ProtectorImg} style={styles.profileImage} />
+            </View>
 
-      <View style={styles.body}>
-        <Text style={styles.title}>{protectorName}</Text>
-        <Text style={styles.description}>{protectorDesc}</Text>
-      </View>
+            <View style={styles.body}>
+              <Text style={styles.title}>{protectorName}</Text>
+              <Text style={styles.description}>{protectorDesc}</Text>
+            </View>
 
-      <View style={styles.infoList}>
-        
-        <Text style={styles.infoItem}><Image style={styles.phone} source={phone}/> +54341567890</Text>
-        <Text style={styles.infoItem}><Image style={styles.phone} source={phone}/> {protectorMail}</Text>
-        <Text style={styles.infoItem}><Image style={styles.phone} source={phone}/> {protectorInstagram}</Text>
-      </View>
-    </View>
+            <View style={styles.infoList}>
+              
+              <Text style={styles.infoItem}><Image style={styles.phone} source={phone}/> +54341567890</Text>
+              <Text style={styles.infoItem}><Image style={styles.phone} source={phone}/> {protectorMail}</Text>
+              <Text style={styles.infoItem}><Image style={styles.phone} source={phone}/> {protectorInstagram}</Text>
+            </View>
+          </View>
+  </View>
   );
 };
 
--- pages/ProtectorCard/protector-card.js
+++ pages/ProtectorCard/protector-card.js
@@ -2,6 +2,12 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
+    container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center', 
+    backgroundColor: '#f8f9fa', 
+    },
     card: {
       width: 280,
       borderRadius: 15,
