8c7a6314c5ab5e09e8b09c242edd43d15acce510
HildaEncina <encina.hilda@gmail.com>
2024-10-30 16:59:40 -0300

    feat(F35) Home search

--- pages/Home/index.jsx
+++ pages/Home/index.jsx
@@ -1,29 +1,55 @@
-import { useState, useRef, useEffect } from 'react';
-import { View, Text, Animated, TouchableOpacity, StyleSheet, Dimensions, Image, ScrollView } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { useState, useRef, useEffect } from "react";
+import {
+  View,
+  Text,
+  Animated,
+  TouchableOpacity,
+  StyleSheet,
+  Dimensions,
+  Image,
+  ScrollView,
+  TextInput,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import { useNavigation } from "@react-navigation/native";
-import { logout } from '../../auth/authSlice';
+import { logout } from "../../auth/authSlice";
 import { getPets, getProtector } from "./petSlice";
-import { useDispatch, useSelector } from 'react-redux';
-import toggleButton from '../../assets/home/bar-chart.png';
-import profile from '../../assets/home/profile.png';
-import add from '../../assets/home/add.png';
-import PetCard from '../PettCard';
-import ProtectorCard from '../ProtectorCard';
+import { useDispatch, useSelector } from "react-redux";
+import toggleButton from "../../assets/home/bar-chart.png";
+import profile from "../../assets/home/profile.png";
+import add from "../../assets/home/add.png";
+import PetCard from "../PettCard";
+import ProtectorCard from "../ProtectorCard";
+import iconSearch from "../../assets/iconSearch.png";
 
-const { width } = Dimensions.get('window');
+const { width } = Dimensions.get("window");
 
 const Home = () => {
   const dispatch = useDispatch();
   const navigation = useNavigation();
   const { token, role } = useSelector((state) => state.login);
-  const { petsAvailable, protectorsAvailable } = useSelector((state) => state.pets);
+  const { petsAvailable, protectorsAvailable } = useSelector(
+    (state) => state.pets
+  );
+
   const [isVisible, setIsVisible] = useState(false);
   const slideAnim = useRef(new Animated.Value(-width)).current;
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filteredPets, setFilteredPets] = useState([]);
+
+  /**Logica del buscador */
+  const handleSearch = () => {
+    const filtered = petsAvailable.filter((pet) =>
+      pet.nombre.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    setFilteredPets(filtered);
+  };
+
+  /** */
 
   const handleLogout = () => {
     dispatch(logout());
-    navigation.navigate('Login');
+    navigation.navigate("Login");
   };
 
   useEffect(() => {
@@ -45,133 +71,194 @@ const Home = () => {
         <View>
           <View style={styles.navbar}>
             <TouchableOpacity onPress={toggleOffcanvas}>
-              <Image style={styles.navbar__btnOffCanvas} source={toggleButton} />
+              <Image
+                style={styles.navbar__btnOffCanvas}
+                source={toggleButton}
+              />
             </TouchableOpacity>
             <Image style={styles.navbar__profileImg} source={profile} />
           </View>
         </View>
 
-        <View>
-          <Text style={{height: 100}}>Acá va el buscador</Text>
+        {/* Buscador */}
+        <View style={styles.searchContainer}>
+          <TextInput
+            style={styles.searchInput}
+            placeholder="Tipo de mascota (e.g., perro, gato)"
+            placeholderTextColor="#D8D8D8"
+            value={searchQuery}
+            onChangeText={setSearchQuery}
+          />
+          <TouchableOpacity onPress={handleSearch}>
+            <Image source={iconSearch} style={{ width: 30, height: 30 }} />
+          </TouchableOpacity>
         </View>
 
         <View>
-          <Text style={{height: 100}}>Acá va slider</Text>
+          <Text style={{ height: 100 }}>Acá va slider</Text>
         </View>
-        
+
         <View>
-          { petsAvailable.length < 1 ? 
-            <Text style={{textAlign: 'center'}}>No hay mascotas disponibles</Text>
-            : 
+          {filteredPets.length > 0 ? ( 
             <ScrollView horizontal>
-              {(petsAvailable.map((pet) => (
-                <PetCard key={pet.id} img={pet.fotos[0]} name={pet.nombre} city={pet.ciudad} sex={pet.sexo} />))
-              )}
+              {filteredPets.map((pet) => (
+                <PetCard
+                  key={pet.id}
+                  img={pet.fotos[0]}
+                  name={pet.nombre}
+                  city={pet.ciudad}
+                  sex={pet.sexo}
+                />
+              ))}
             </ScrollView>
-          }
+          ) : petsAvailable.length > 0 ? (
+            <ScrollView horizontal>
+              {petsAvailable.map((pet) => (
+                <PetCard
+                  key={pet.id}
+                  img={pet.fotos[0]}
+                  name={pet.nombre}
+                  city={pet.ciudad}
+                  sex={pet.sexo}
+                />
+              ))}
+            </ScrollView>
+          ) : (
+            <Text style={{ textAlign: "center" }}>
+              No hay mascotas disponibles
+            </Text> 
+          )}
         </View>
-
         <View>
           <ScrollView horizontal>
-            {(protectorsAvailable.map((protector) => (
+            {protectorsAvailable.map((protector) => (
               <ProtectorCard
                 key={protector.id}
                 protectorName={protector.nombre}
                 protectorDesc={protector.descripcion}
                 protectorMail={protector.email}
-                protectorInstagram={protector.instagram}  
-              />))
-            )}
+                protectorInstagram={protector.instagram}
+              />
+            ))}
           </ScrollView>
         </View>
 
-        {role === 'Protectora' && (
-          <TouchableOpacity onPress={() => navigation.navigate('PettAdd')} style={styles.addBtn}>
+        {role === "Protectora" && (
+          <TouchableOpacity
+            onPress={() => navigation.navigate("PettAdd")}
+            style={styles.addBtn}
+          >
             <Image source={add} />
           </TouchableOpacity>
         )}
       </SafeAreaView>
 
-      <Animated.View style={[styles.offcanvas, { transform: [{ translateX: slideAnim }] }]}>
+      <Animated.View
+        style={[styles.offcanvas, { transform: [{ translateX: slideAnim }] }]}
+      >
         <Text style={styles.offcanvasText}>App creada por Hilda y Seba</Text>
         <TouchableOpacity onPress={toggleOffcanvas} style={styles.closeButton}>
           <Text style={styles.closeText}>Cerrar</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={{ backgroundColor: 'red', padding: 10, width: 200, marginTop: 20, marginBottom: 20  }} onPress={handleLogout}>
-           <Text>Cerrar sesión</Text>
-         </TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            backgroundColor: "red",
+            padding: 10,
+            width: 200,
+            marginTop: 20,
+            marginBottom: 20,
+          }}
+          onPress={handleLogout}
+        >
+          <Text>Cerrar sesión</Text>
+        </TouchableOpacity>
       </Animated.View>
-
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f0f0f0',
+    backgroundColor: "#f0f0f0",
   },
-  safeArea : {
-    position: 'relative',
-    height: '%100',
+  safeArea: {
+    position: "relative",
+    height: "%100",
   },
-  addBtn : {
-    position: 'absolute',
+  addBtn: {
+    position: "absolute",
     bottom: 30,
     right: 10,
   },
-  navbar : {
-    display: 'flex',
+  navbar: {
+    display: "flex",
     marginTop: 5,
     paddingLeft: 20,
     paddingRight: 15,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
-  navbar__profileImg : {
+  navbar__profileImg: {
     width: 65,
     height: 65,
   },
-  navbar__btnOffCanvas : {
+  navbar__btnOffCanvas: {
     width: 40,
     height: 40,
   },
   button: {
     padding: 10,
-    backgroundColor: '#007bff',
+    backgroundColor: "#007bff",
     borderRadius: 5,
   },
   buttonText: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 16,
   },
   offcanvas: {
-    position: 'absolute',
+    position: "absolute",
     top: 0,
     left: 0,
-    width: width * 0.8, // Ancho del offcanvas
-    height: '100%',
-    backgroundColor: 'white',
+    width: width * 0.8,
+    height: "100%",
+    backgroundColor: "white",
     padding: 20,
-    justifyContent: 'center',
+    justifyContent: "center",
   },
   offcanvasText: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 18,
     marginBottom: 20,
   },
   closeButton: {
     marginTop: 20,
     padding: 10,
-    backgroundColor: '#ff4757',
+    backgroundColor: "#ff4757",
     borderRadius: 5,
   },
   closeText: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 16,
+  },
+  searchContainer: {
+    marginHorizontal: 20,
+    marginVertical: 10,
+    borderRadius: 8,
+    backgroundColor: "#f7f7f7",
+    flexDirection: "row",
+    alignItems: "center",
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderColor: "#e0e0e0",
+  },
+  searchInput: {
+    flex: 1,
+    height: 40,
+    fontSize: 16,
+    color: "#333",
   },
 });
 
-
-export default Home
+export default Home;
