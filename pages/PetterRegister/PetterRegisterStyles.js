3844917f748fcdb79ac1c2e600a275c05149663d
Seba Luna <ramon.sebastian.luna@gmail.com>
2024-11-01 03:24:34 -0300

    fix: error validation in forms

--- pages/PetterRegister/PetterRegisterStyles.js
+++ pages/PetterRegister/PetterRegisterStyles.js
@@ -6,24 +6,19 @@ const { width } = Dimensions.get('window');
 const styles = StyleSheet.create({
 
   container: {
-
- 
     justifyContent:"center", 
     alignItems: 'center',
     marginTop:80,
-   
-},
-  
- container_input: {
-  width:"80%"
- },
+  },
+  container_input: {
+    width:"80%"
+  },
   logo: {
     width: 106.67,
     height: 99,
     marginTop: 120,
     marginBottom: 50,
   },
-
   button: {
     justifyContent: 'center',
     marginTop: 50,
@@ -32,22 +27,16 @@ const styles = StyleSheet.create({
     padding: 15,
     borderRadius: 10,
     alignItems: 'center',
-   
-    
   },
   buttonText:{
     color:"#F6F6F6",
-
   },
-
   input: {
     backgroundColor: '#F6F6F6', 
     borderWidth: 1,
     borderColor: "#ccc",
     padding: 10,
-    borderRadius: 8,
-    marginBottom: 15,
-   
+    borderRadius: 8, 
   },
   passwordContainer: {
     position: 'relative', 
@@ -57,25 +46,33 @@ const styles = StyleSheet.create({
     right: 10,
     width: 27,
     height: 20,
-    top: '100%',
+    top: '130%',
     transform: [{ translateY: -50 }],
   },
   icon: {
     width: 27,
     height: 20,
   },
-
   forgotPassword: {
     color: 'var.$secondary-01', 
     textDecorationLine: 'underline',
     fontSize: 14,
     marginLeft: 'auto',
   },
-
   error: {
     color: "red",
     fontSize: 12,
     marginBottom: 20,
+  },
+  inputError: {
+    borderColor: "red",
+  },
+  errorText: {
+    color: "red",
+    fontSize: 12,
+  },
+  inputGroup: {
+    marginBottom: 15,
   },
 });
 
